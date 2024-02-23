5641234e867a7f35ad63165d8743eab691879b5d
Tadej Vengust <vengust.tadej@gmail.com>
2024-02-23 16:45:23 +0100

    add api base

--- frontend/lib/config/production.ts
+++ frontend/lib/config/production.ts
@@ -2,7 +2,7 @@ import { ConfigInterface } from '~/lib/types/general.types';
 
 const config: ConfigInterface = {
   APP_URL: 'https://app.apillon.io',
-  API_BASE: 'http://64.227.118.87',
+  API_BASE: 'https://demo-api.web3approved.com',
   CAPTCHA_KEY: 'f2dfcbd7-809e-4fa3-a45f-539b460a1ca0',
 };
 
