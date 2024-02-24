4ffa140e9fe976607d89143ef41b797c067963c6
Tadej Vengust <vengust.tadej@gmail.com>
2024-02-24 11:58:39 +0100

    update contract address

--- frontend/lib/config/production.ts
+++ frontend/lib/config/production.ts
@@ -5,7 +5,7 @@ const config: ConfigInterface = {
   API_BASE: 'https://demo-api.web3approved.com',
   CAPTCHA_KEY: 'f2dfcbd7-809e-4fa3-a45f-539b460a1ca0',
   CHAIN_ID: 1287, // testnet 1284 moonbeam mainnet
-  CONTRACT_ADDRESS: '0x5D1c18DF2BE40D82b40b83c37F344C8764fBC7B7',
+  CONTRACT_ADDRESS: '0x121E354BE75270f0418fb87aA0863Bee6f31CE67',
 };
 
 export default config;
