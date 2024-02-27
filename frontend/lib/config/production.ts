33a5644c3de0cf260b2441c42d07ef36eaf7deb3
Tadej Vengust <vengust.tadej@gmail.com>
2024-02-27 14:28:20 -0700

    update production config

--- frontend/lib/config/production.ts
+++ frontend/lib/config/production.ts
@@ -4,8 +4,8 @@ const config: ConfigInterface = {
   APP_URL: 'https://app.apillon.io',
   API_BASE: 'https://demo-api.web3approved.com',
   CAPTCHA_KEY: 'f2dfcbd7-809e-4fa3-a45f-539b460a1ca0',
-  CHAIN_ID: 1287, // testnet 1284 moonbeam mainnet
-  CONTRACT_ADDRESS: '0x121E354BE75270f0418fb87aA0863Bee6f31CE67',
+  CHAIN_ID: 1284, // 1287 testnet 1284 moonbeam mainnet
+  CONTRACT_ADDRESS: '0x956da55d8Ab62711a3C62d24219df1197b14e943',
 };
 
 export default config;
